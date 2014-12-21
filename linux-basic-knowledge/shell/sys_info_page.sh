c34fa2e8340577600816b9223ac7e340106d2add
beyondkoma <850239158@qq.com>
2014-12-21 22:13:02 +0800

    little modify

---
+++ linux-basic-knowledge/shell/sys_info_page.sh
@@ -0,0 +1,17 @@
+#！ /bin/sh
+
+# Program to out put a system information page
+
+declare -r TITLE="System Information Report For $HOSTNAME"
+CURRENT_TIME=$(date +"%x %r %z")
+TIME_STAMP="Generated $CURRENT_TIME, by $USER"
+
+echo  "<HTML>
+     <HEAD>
+             <TITLE>$TITLE</TITLE>
+     </HEAD>
+       <BODY>
+            <H1>$TITLE</H1>
+           <P>$TIME_STAMP</P>
+       </BODY>
+</HTML>"
