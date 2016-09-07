a6f10ef1624973892f822222709417288992b12b
beyondkoma <850239158@qq.com>
2016-09-07 19:13:47 +0800

    three examples about the difference between yield and yield from

--- pythonKnowledge/yield&&yieldfrom.py
+++ pythonKnowledge/yield&&yieldfrom.py
@@ -43,9 +43,73 @@ def writer_wrapper(coro):
         except StopIteration:
             pass
 
+        
+def writer_wrapper_from(coro):
+    yield from coro
+
+    
+# w = writer()
+# wrap = writer_wrapper(w)
+# wrap.send(None)  # "prime" the coroutine
+# for i in range(4):
+#     wrap.send(i)
+
+
+# w = writer()
+# wrap_from = writer_wrapper_from(w)
+# wrap_from.send(None)  # "prime" the coroutine
+# for i in range(4):
+#     wrap_from.send(i)    
+
+# Sending data to a generator yield from - Part 2 - Exception handling    
+
+def writer_wrapper(coro):
+    """Works. Manually catches exceptions and throws them"""
+    coro.send(None)  # prime the coro
+    while True:
+        try:
+            try:
+                x = (yield)
+            except Exception as e:   # This catches the SpamException
+                coro.throw(e)
+            else:
+                coro.send(x)
+        except StopIteration:
+            pass
+
+        
+class SpamException(Exception):
+    pass
+
+def writer():
+    while True:
+        try:
+            w = (yield)
+        except SpamException:
+            print('***')
+        else:
+            print('>> ', w)
+
 
 w = writer()
 wrap = writer_wrapper(w)
 wrap.send(None)  # "prime" the coroutine
-for i in range(4):
-    wrap.send(i)        
+for i in [0, 1, 2, 'spam', 4]:
+    if i == 'spam':
+        wrap.throw(SpamException)
+    else:
+        wrap.send(i)
+
+def writer_wrapper(coro):
+    yield from coro
+
+    
+w = writer()
+wrap_from = writer_wrapper(w)
+wrap_from.send(None)  # "prime" the coroutine
+for i in [0, 1, 2, 'spam', 4]:
+    if i == 'spam':
+        wrap_from.throw(SpamException)
+    else:
+        wrap_from.send(i)        
+            
